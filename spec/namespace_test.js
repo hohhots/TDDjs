b1f032836853d3f59815c2dd628ac26087ed85e1
brgd <hohhots@gmail.com>
2013-04-19 16:34:01 +0800

    lib/tdd.js

--- spec/namespace_test.js
+++ spec/namespace_test.js
@@ -1,1 +1,35 @@
-var tddjs = (function(){}());
+describe("Test namespace of tddjs",function(){
+    afterEach(function(){
+	delete tddjs.nstest;
+    });
+
+    it('Should create none-existent object', function () {
+	tddjs.namespace("nstest");
+	 
+	expect(tddjs.nstest).toBeDefined();  
+    });
+
+    it('Should not overwrite existing objects', function () {
+	tddjs.nstest = {nested:{}};
+	var result = tddjs.namespace("nstest.nested");
+
+    	expect(tddjs.nstest.nested).toBe(result);  
+    });
+
+    it('Should only create missing parts', function () {
+	var existing = {}; 
+	tddjs.nstest = {nested:{existing:existing}};
+	var result = tddjs.namespace("nstest.nested.ui");
+
+	expect(tddjs.nstest.nested.existing).toBe(existing);
+	expect(tddjs.nstest.nested.ui).toBeDefined(); 
+    });
+
+    it('test namespacing inside other objects', function () {
+	var custom = {namespace:tddjs.namespace};
+	custom.namespace("dom.event");
+
+	expect(custom.dom.event).toBeDefined();
+	expect(tddjs.dom).toBeUndefined();
+    });
+});
