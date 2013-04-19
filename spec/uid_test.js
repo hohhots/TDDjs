65f7972315d1da0d31fe46de8ebecbfe24a910c1
brgd <hohhots@gmail.com>
2013-04-20 01:21:38 +0800

    uid

--- spec/uid_test.js
+++ spec/uid_test.js
@@ -3,6 +3,34 @@ describe("test uid",function(){
     it('Should return numeric id', function () {
 	var id = tddjs.uid({});
 	
-	expect(!isNaN(id)).toBeTruthy();
+	expect(isNaN(id)).toBeFalsy();
+    });
+
+    it('Should return consistent id for object', function () {
+	var object = {};
+	var id = tddjs.uid(object);
+
+	expect(tddjs.uid(object)).toEqual(id);
+    });
+
+    it('Should return unique id', function () {
+	var object = {};
+	var object2 = {};
+	var id = tddjs.uid(object);
+
+	expect(tddjs.uid(object2)).not.toEqual(id);
+    });
+
+    it('Should return consistent id for function', function () {
+	var func = function(){};
+	var id = tddjs.uid(func);
+
+	expect(tddjs.uid(func)).toEqual(id);
+    });
+
+    it('Should return undefined for primitive', function () {
+	var str = "my string";
+
+	expect(tddjs.uid(str)).toBeUndefined();
     });
 });
