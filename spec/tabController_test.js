eff3378a55fdaa858bcaa045014cb4befcc93869
brgd <hohhots@gmail.com>
2013-04-24 23:17:08 +0800

    spec/tabController_test.js

--- spec/tabController_test.js
+++ spec/tabController_test.js
@@ -1,26 +1,27 @@
 (function(){
     var tabController = tddjs.ui.tabController;
 
-    function setUp(){
-	/*DOC += <ol id="tabs">
-	  
-	  </ol>*/
-
-	this.tabs = document.getElementById("tabs");
-    }
-
     describe("test tab controller create",function(){
-	beforeEach(setUp);
+	var fixture;
+	
+	beforeEach(function(){
+	    fixture = loadFixtures(setFixtures('<ol id="tabs"><li><a href="#news">News</a></li><li><a href="#sports">Sports</a></li><li><a href="#economy">Economy</a></li></ol>'));
+
+	    this.tabs = document.getElementById("tabs");
+	});
 
 	it('Should fail without element', function () {
+	    fixture.myTestedJqueryPlugin();
+
 	    expect(function(){
 		tabController.create();
 	    }).toThrow(new TypeError("element is not an element"));
 	});
 
 	it('Should fail without element class', function () {
-	    expect(
-		function(){
+	    fixture.myTestedJqueryPlugin();
+
+	    expect(function(){
 		    tabController.create({});
 		}).toThrow(new TypeError("element is not an element"));
 	});
