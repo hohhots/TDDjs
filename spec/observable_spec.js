3006a708acd098f96efc6adb65cc14d6d28f2fd7
brgd <hohhots@gmail.com>
2013-04-27 02:12:45 +0800

    observable_spec.js

--- spec/observable_spec.js
+++ spec/observable_spec.js
@@ -1,10 +1,88 @@
+var observable;
+
+beforeEach(function(){
+    observable = new tddjs.util.Observable();
+});
+
 describe("observable add observer test",function(){
+
     it('Should store function', function () {
-	var observable = new tddjs.util.Observable();
+	var observers = [function(){},function(){}];
+
+	observable.addObserver(observers[0]);
+	observable.addObserver(observers[1]);
+
+	expect(observable.hasObserver(observers[0])).toBeTruthy();
+	expect(observable.hasObserver(observers[1])).toBeTruthy();
+    });
+
+});
+
+describe("test observable has observer",function(){
+    it('Should return true when has observer', function () {
 	var observer = function(){};
 
 	observable.addObserver(observer);
+	expect(observable.hasObserver(observer)).toBeTruthy();
+    });
 
-	expect(observable.observers[0]).toEqual(observer);
+    it('Should return false when no observers', function () {
+	expect(observable.hasObserver(function(){})).toBeFalsy();
+    });
+});
+
+describe("test observable notify observers",function(){
+    it('Should call all observers', function () {
+	var observer1 = function(){observer1.called = true;};
+	var observer2 = function(){observer2.called = true;};
+
+	observable.addObserver(observer1);
+	observable.addObserver(observer2);
+	observable.notifyObservers();
+
+	expect(observer1.called).toBeTruthy();
+	expect(observer2.called).toBeTruthy();
+    });
+
+    it('Should pass through arguments', function () {
+	var actual;
+	
+	observable.addObserver(function(){
+	    actual = arguments;
+	});
+
+	observable.notifyObservers("String",1,32);
+
+	expect(actual).toEqual({0:"String",1:1,2:32});
+    });
+
+    it('Should throw for uncallable observer', function () {
+	expect(function(){
+	    observable.addObserver({});
+	}).toThrow();
+    });
+
+    it('Should notify all even when some fail', function () {
+	var observer1 = function(){throw new Error("Oops");};
+	var observer2 = function(){observer2.called = true;};
+
+	observable.addObserver(observer1);
+	observable.addObserver(observer2);
+	observable.notifyObservers();
+
+	expect(observer2.called).toBeTruthy();
+    });
+
+    it('Should call observers in the order they were added', function () {
+	var calls = [];
+	var observer1 = function(){calls.push(observer1);};
+	var observer2 = function(){calls.push(observer2);};
+	observable.addObserver(observer1);
+	observable.addObserver(observer2);
+
+	observable.notifyObservers();
+
+	expect(calls[0]).toEqual(observer1);
+	expect(calls[1]).toEqual(observer2);
     });
 });
--- src/observable.js
+++ src/observable.js
@@ -2,14 +2,38 @@ tddjs.namespace("util");
 
 (function(){
     function Observable(){
-	
+	this.observers = [];
     }
 
     function addObserver(observer){
-	this.observers = [observer];
-    }
-    Observable.prototype.addObserver = addObserver;
+	if(typeof observer != "function"){
+	    throw new TypeError("observer is not function");
+	}
 
+	this.observers.push(observer);
+    }
+
+    function hasObserver(observer){
+	for( var i=0,l=this.observers.length; i < l; i++){
+	    if(this.observers[i] == observer){
+		return true;
+	    }
+	}
+
+	return false;
+    }
+
+    function notifyObservers(){
+	for( var i=0,l=this.observers.length; i < l; i++){
+	    try{
+		this.observers[i].apply(this,arguments);
+	    }catch(e){}
+	}
+    }
+
+    Observable.prototype.addObserver = addObserver;
+    Observable.prototype.hasObserver = hasObserver;
+    Observable.prototype.notifyObservers = notifyObservers;
 
     tddjs.util.Observable = Observable;
 }());
