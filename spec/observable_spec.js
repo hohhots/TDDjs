c1bf56fa02e2784db347cb4b88be130a0b766ef8
brgd <hohhots@gmail.com>
2013-04-28 17:05:12 +0800

    spec/observable_spec.js src/observable.js

--- spec/observable_spec.js
+++ spec/observable_spec.js
@@ -1,7 +1,5 @@
-var observable;
-
 beforeEach(function(){
-    observable = new tddjs.util.Observable();
+    this.observable = Object.create(tddjs.util.observable);
 });
 
 describe("observable add observer test",function(){
@@ -9,11 +7,11 @@ describe("observable add observer test",function(){
     it('Should store function', function () {
 	var observers = [function(){},function(){}];
 
-	observable.addObserver(observers[0]);
-	observable.addObserver(observers[1]);
+	this.observable.observe("event",observers[0]);
+	this.observable.observe("event",observers[1]);
 
-	expect(observable.hasObserver(observers[0])).toBeTruthy();
-	expect(observable.hasObserver(observers[1])).toBeTruthy();
+	expect(this.observable.hasObserver("event",observers[0])).toBeTruthy();
+	expect(this.observable.hasObserver("event",observers[1])).toBeTruthy();
     });
 
 });
@@ -22,12 +20,12 @@ describe("test observable has observer",function(){
     it('Should return true when has observer', function () {
 	var observer = function(){};
 
-	observable.addObserver(observer);
-	expect(observable.hasObserver(observer)).toBeTruthy();
+	this.observable.observe("event",observer);
+	expect(this.observable.hasObserver("event",observer)).toBeTruthy();
     });
 
     it('Should return false when no observers', function () {
-	expect(observable.hasObserver(function(){})).toBeFalsy();
+	expect(this.observable.hasObserver("event",function(){})).toBeFalsy();
     });
 });
 
@@ -36,29 +34,33 @@ describe("test observable notify observers",function(){
 	var observer1 = function(){observer1.called = true;};
 	var observer2 = function(){observer2.called = true;};
 
-	observable.addObserver(observer1);
-	observable.addObserver(observer2);
-	observable.notifyObservers();
+	this.observable.observe("event",observer1);
+	this.observable.observe("event",observer2);
+	this.observable.notify("event");
 
 	expect(observer1.called).toBeTruthy();
 	expect(observer2.called).toBeTruthy();
     });
 
+    it('Should not fail if no observers', function () {
+	expect(this.observable.notify).not.toThrow();
+    });
+
     it('Should pass through arguments', function () {
 	var actual;
 	
-	observable.addObserver(function(){
+	this.observable.observe("event",function(){
 	    actual = arguments;
 	});
 
-	observable.notifyObservers("String",1,32);
+	this.observable.notify("event","String",1,32);
 
 	expect(actual).toEqual({0:"String",1:1,2:32});
     });
 
     it('Should throw for uncallable observer', function () {
 	expect(function(){
-	    observable.addObserver({});
+	    this.observable.observe("event",{});
 	}).toThrow();
     });
 
@@ -66,9 +68,9 @@ describe("test observable notify observers",function(){
 	var observer1 = function(){throw new Error("Oops");};
 	var observer2 = function(){observer2.called = true;};
 
-	observable.addObserver(observer1);
-	observable.addObserver(observer2);
-	observable.notifyObservers();
+	this.observable.observe("event",observer1);
+	this.observable.observe("event",observer2);
+	this.observable.notify("event");
 
 	expect(observer2.called).toBeTruthy();
     });
@@ -77,12 +79,28 @@ describe("test observable notify observers",function(){
 	var calls = [];
 	var observer1 = function(){calls.push(observer1);};
 	var observer2 = function(){calls.push(observer2);};
-	observable.addObserver(observer1);
-	observable.addObserver(observer2);
+	this.observable.observe("event",observer1);
+	this.observable.observe("event",observer2);
 
-	observable.notifyObservers();
+	this.observable.notify("event");
 
 	expect(calls[0]).toEqual(observer1);
 	expect(calls[1]).toEqual(observer2);
+    });
+
+    it('Should notify relevant observers only', function () {
+	var calls = [];
+	
+	this.observable.observe("event",function(){
+	    calls.push("event");
+	});
+
+	this.observable.observe("other",function(){
+	    calls.push("other");
+	});
+
+	this.observable.notify("other");
+	
+	expect(calls).toEqual(["other"]);
     });
 });
